65f95f651bbb219a02bf7802d5b76ae02a02263e
Harrison Resnick <haresnick@packer.edu>
2020-06-11 19:39:17 -0400

    gets assignments

--- Planner/AppDelegate.swift
+++ Planner/AppDelegate.swift
@@ -9,44 +9,32 @@
 import UIKit
 import GoogleSignIn
 
-
 @UIApplicationMain
-// [START appdelegate_interfaces]
+
 class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
-    //  var myAuth: GTMFetcherAuthorizationProtocol? = nil
-  // [END appdelegate_interfaces]
+
   var window: UIWindow?
     
-
-
   // [START didfinishlaunching]
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    // Initialize sign-in
-   
 
     GIDSignIn.sharedInstance().clientID = "136124065580-737s6o2l8t0jsj975oskchvr2jrmpljq.apps.googleusercontent.com"
     GIDSignIn.sharedInstance().delegate = self
 
     return true
   }
-  // [END didfinishlaunching]
 
-  // [START openurl]
   func application(_ application: UIApplication,
                    open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
     return GIDSignIn.sharedInstance().handle(url)
   }
-  // [END openurl]
 
-  // [START openurl_new]
   @available(iOS 9.0, *)
   func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
     return GIDSignIn.sharedInstance().handle(url)
   }
-  // [END openurl_new]
-
-  // [START signin_handler]
+    
   func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!,
             withError error: Error!) {
 
@@ -57,38 +45,28 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
       } else {
         print("\(error.localizedDescription)")
       }
-      // [START_EXCLUDE silent]
       NotificationCenter.default.post(
         name: Notification.Name(rawValue: "ToggleAuthUINotification"), object: nil, userInfo: nil)
-      // [END_EXCLUDE]
       return
     }
-    // Perform any operations on signed in user here.
 //    let userId = user.userID                  // For client-side use only!
 //    let idToken = user.authentication.idToken // Safe to send to the server
-    let fullName = user.profile.name
+      let fullName = user.profile.name
 //    let givenName = user.profile.givenName
 //    let familyName = user.profile.familyName
 //    let email = user.profile.email
-    // [START_EXCLUDE]
     NotificationCenter.default.post(
       name: Notification.Name(rawValue: "ToggleAuthUINotification"),
       object: nil,
       userInfo: ["statusText": "Signed in user:\n\(fullName!)"])
-    // [END_EXCLUDE]
   }
-  // [END signin_handler]
 
-  // [START disconnect_handler]
   func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!,
             withError error: Error!) {
-    // Perform any operations when the user disconnects from app here.
-    // [START_EXCLUDE]
+
     NotificationCenter.default.post(
       name: Notification.Name(rawValue: "ToggleAuthUINotification"),
       object: nil,
       userInfo: ["statusText": "User has disconnected."])
-    // [END_EXCLUDE]
   }
-  // [END disconnect_handler]
 }
--- Planner/ViewController.swift
+++ Planner/ViewController.swift
@@ -10,105 +10,110 @@ import UIKit
 import GoogleSignIn
 
 class ViewController: UIViewController, GIDSignInDelegate {
+    
     var myAuth: GTMFetcherAuthorizationProtocol? = nil
+    private let service = GTLRClassroomService()
+    var classIds = [String]()
+    var classNames = [String]()
+    
     func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
 
-            // Logged into google services successfully!
-            // Save relevant details from user.authentication to refresh the token when needed.
-
-            // Set GTMOAuth2Authentication authoriser for your Google Drive service
             GIDSignIn.sharedInstance().delegate=self
             GIDSignIn.sharedInstance().scopes = scopes
             myAuth = user.authentication.fetcherAuthorizer()
-         //   myAuth = user.authentication.fetcherAuthorizer()
- 
-        
-        
     }
     
     private let scopes = [OIDScopeEmail, OIDScopeProfile, OIDScopeOpenID, "https://www.googleapis.com/auth/classroom.student-submissions.students.readonly", "https://www.googleapis.com/auth/classroom.courses.readonly", "https://www.googleapis.com/auth/classroom.rosters.readonly", "https://www.googleapis.com/auth/classroom.student-submissions.me.readonly", "https://www.googleapis.com/auth/classroom.coursework.me"]
 
-    
-    private let service = GTLRClassroomService()
-    
     func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!) {
         myAuth = nil
     }
  
-
     func fetchCourses() {
-        
-        
-        GIDSignIn.sharedInstance().delegate=self
-        GIDSignIn.sharedInstance().scopes = scopes
-
-        print("Getting classes...")
 
         let query = GTLRClassroomQuery_CoursesList.query()
-
         query.pageSize = 30
-
+        query.executionParameters.shouldFetchNextPages = true
         service.executeQuery(query,
-
                              delegate: self,
-                             didFinish: #selector(displayResultWithTicket(ticket:finishedWithObject:error:)))
+                             didFinish: #selector(obtainClassIds(ticket:finishedWithObject:error:)))
 
     }
-
-    @objc func displayResultWithTicket(ticket: GTLRServiceTicket,
-
-                                 finishedWithObject result : GTLRClassroom_ListCoursesResponse,
-
-                                 error : NSError?) {
-
-        if let error = error {
-
-            // showAlert(title: "Error", message: error.localizedDescription)
-            print(error.localizedDescription)
-
-            return
-
+    
+    func fetchAssignments() {
+        
+        for classID in 0...classIds.endIndex-1 {
+        
+            if classIds != [] {
+                let query = GTLRClassroomQuery_CoursesCourseWorkList.query(withCourseId: classIds[classID])
+                query.pageSize = 30
+                query.executionParameters.shouldFetchNextPages = true
+                service.executeQuery(query,
+                                     delegate: self,
+                                     didFinish: #selector(obtainClasses))
+            } else {
+                print("Obtain classes first")
+            }
         }
+        
+    }
+    
+    @objc func obtainClasses(ticket: GTLRServiceTicket,
+                                 finishedWithObject result : GTLRClassroom_ListCourseWorkResponse,
+                                 error : NSError?) {
+        if let error = error {
+            print(error.localizedDescription)
+            return
+        }
+        
+        guard let assignments = result.courseWork, !assignments.isEmpty else {
+            print("No assignments.")
+            return
+        }
+        
+        var outputText = ""
+        
+        for assignment in assignments {
+            outputText += "\(assignment.title ?? "No title")\n"
+        }
+          print(outputText)
+    }
 
     
-        guard let courses = result.courses, !courses.isEmpty else {
 
-            print("No courses.")
-
+    @objc func obtainClassIds(ticket: GTLRServiceTicket,
+                                 finishedWithObject result : GTLRClassroom_ListCoursesResponse,
+                                 error : NSError?) {
+        if let error = error {
+            print(error.localizedDescription)
             return
-
+        }
+        
+        guard let courses = result.courses, !courses.isEmpty else {
+            print("No courses.")
+            return
         }
 
         var outputText = "Courses:\n"
 
         for course in courses {
-
-            // The API field "id" is renamed "identifier" in this library.
-
             outputText += "\(course.name ?? "") (\(course.identifier!))\n"
-
+            classIds.append(course.identifier!)
+            classNames.append(course.name!)
         }
-
         print(outputText)
-
+        print(classIds)
     }
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
     GIDSignIn.sharedInstance()?.presentingViewController = self
-
-    // Automatically sign in the user.
-    //GIDSignIn.sharedInstance()?.restorePreviousSignIn()
-    
     GIDSignIn.sharedInstance().scopes = scopes
-    
     service.authorizer = myAuth
-    // [END_EXCLUDE]
+    
   }
-    
-    
-
+  
     @IBAction func signOut(_ sender: Any) {
         GIDSignIn.sharedInstance().signOut()
         GIDSignIn.sharedInstance().disconnect()
@@ -129,6 +134,19 @@ class ViewController: UIViewController, GIDSignInDelegate {
         }
         service.authorizer = myAuth
         fetchCourses()
+        
+    }
+    
+    @IBAction func getAssignments(_ sender: Any) {
+
+        if GIDSignIn.sharedInstance()?.currentUser != nil {
+            
+            myAuth = GIDSignIn.sharedInstance()?.currentUser.authentication.fetcherAuthorizer()
+        } else {
+            myAuth = nil
+        }
+        service.authorizer = myAuth
+        fetchAssignments()
         
     }
     
