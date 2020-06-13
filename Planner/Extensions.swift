ae039a1e9eb25a020476b1ce07f9751837516395
Harrison Resnick <haresnick@packer.edu>
2020-06-13 18:16:28 -0400

    class name and assignments in dictionary

---
+++ Planner/Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  Extensions.swift
+//  Planner
+//
+//  Created by Harrison Resnick on 6/13/20.
+//  Copyright © 2020 Harrison Resnick. All rights reserved.
+//
+
+import Foundation
+
+extension Array {
+    func arrayWithoutFirstElement() -> Array {
+        if count != 0 { // Check if Array is empty to prevent crash
+            var newArray = Array(self)
+            newArray.removeFirst()
+            return newArray
+        }
+        return []
+    }
+}
--- Planner/ViewController.swift
+++ Planner/ViewController.swift
@@ -17,21 +17,23 @@ class ViewController: UIViewController, GIDSignInDelegate {
     var assignmentsPerCourse = [Array<String>]()
     var assignmentIndex = 0
     
-    var classIDAndName = [String:String]()
+    var classIDAndName = [String : String]()
+    var classNameAndAssignments = [String : Array<String>]()
     
     let date = Date()
     var calendar = Calendar.current
+    
+    private let scopes = [OIDScopeEmail, OIDScopeProfile, OIDScopeOpenID,kGTLRAuthScopeClassroomStudentSubmissionsStudentsReadonly, kGTLRAuthScopeClassroomCoursesReadonly, kGTLRAuthScopeClassroomRostersReadonly, kGTLRAuthScopeClassroomCourseworkMe]
     
     @IBOutlet weak var textViewTest: UITextView!
     
     func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
 
-            GIDSignIn.sharedInstance().delegate=self
+            GIDSignIn.sharedInstance().delegate = self
             GIDSignIn.sharedInstance().scopes = scopes
             myAuth = user.authentication.fetcherAuthorizer()
     }
-    
-    private let scopes = [OIDScopeEmail, OIDScopeProfile, OIDScopeOpenID,kGTLRAuthScopeClassroomStudentSubmissionsStudentsReadonly, kGTLRAuthScopeClassroomCoursesReadonly, kGTLRAuthScopeClassroomRostersReadonly, kGTLRAuthScopeClassroomCourseworkMe]
+
 
     func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!) {
         myAuth = nil
@@ -64,11 +66,13 @@ class ViewController: UIViewController, GIDSignInDelegate {
                                      delegate: self,
                                      didFinish: #selector(obtainClasses))
                 
+                
             } else {
                 print("Obtain classes first")
+                break
             }
         }
-        
+        textViewTest.text = "Information fetched"
     }
         
     @objc func obtainClasses(ticket: GTLRServiceTicket,
@@ -84,22 +88,19 @@ class ViewController: UIViewController, GIDSignInDelegate {
             return
         }
         
-        
-        var outputText = ""
-      
-        let currentMonth = calendar.component(.month, from: date)
-        let currentDay = calendar.component(.day, from: date)
-        let currentYear = calendar.component(.year, from: date)
+//        let currentMonth = calendar.component(.month, from: date)
+//        let currentDay = calendar.component(.day, from: date)
+//        let currentYear = calendar.component(.year, from: date)
         
         //  for classCount in 0...classNames.count - 1 {
          
             for assignment in assignments {
                 
-                let dueMonth = assignment.dueDate?.month as? Int
-                let dueDay = assignment.dueDate?.day as? Int
-                let dueYear = assignment.dueDate?.year as? Int
+//                let dueMonth = assignment.dueDate?.month as? Int
+//                let dueDay = assignment.dueDate?.day as? Int
+//                let dueYear = assignment.dueDate?.year as? Int
                 
-                outputText += "Title: \(assignment.title ?? "No title")\nDue Date: \(dueMonth ?? 0)/\(dueDay ?? 0)/\(dueYear ?? 0)\n"
+                //outputText += "Title: \(assignment.title ?? "No title")\nDue Date: \(dueMonth ?? 0)/\(dueDay ?? 0)/\(dueYear ?? 0)\n"
                 
                 if assignmentsPerCourse[assignmentIndex].count == 0 {
                     assignmentsPerCourse[assignmentIndex].append(classIDAndName[assignment.courseId ?? "0"] ?? "No name")
@@ -107,23 +108,24 @@ class ViewController: UIViewController, GIDSignInDelegate {
 
                 assignmentsPerCourse[assignmentIndex].append(assignment.title ?? "No title")
 
-                if dueYear ?? 100 >= currentYear {
-                    if dueMonth ?? 100 >= currentMonth {
-                        if dueDay ?? 100 >= currentDay {
-                            
-                          //  print(assignment.title ?? "no title")
-                            //Append these assignments to an array --> this is what the user will be able to see
-                            
-                        }
-                    }
-                    //       }
-                
-                    
-            }
+//                if dueYear ?? 100 >= currentYear {
+//                    if dueMonth ?? 100 >= currentMonth {
+//                        if dueDay ?? 100 >= currentDay {
+//
+//                          //  print(assignment.title ?? "no title")
+//                            //Append these assignments to an array --> this is what the user will be able to see
+//
+//                        }
+//                    }
+//                    //       }
+//
+//
+//            }
             //    print(assignmentsPerCourse)
            // break
         }
         assignmentIndex += 1
+
         //print(outputText)
     }
 
@@ -149,6 +151,7 @@ class ViewController: UIViewController, GIDSignInDelegate {
         }
     //    print(outputText)
         textViewTest.text = outputText
+        fetchAssignments()
     }
 
   override func viewDidLoad() {
@@ -166,6 +169,7 @@ class ViewController: UIViewController, GIDSignInDelegate {
         assignmentsPerCourse = [Array<String>]()
         assignmentIndex = 0
         classIDAndName = [String:String]()
+        classNameAndAssignments = [String: Array<String>]()
         
         GIDSignIn.sharedInstance().signOut()
         GIDSignIn.sharedInstance().disconnect()
@@ -176,7 +180,7 @@ class ViewController: UIViewController, GIDSignInDelegate {
         service.authorizer = myAuth
     }
     
-    @IBAction func getClasses(_ sender: Any) {
+    @IBAction func getInfo(_ sender: Any) {
 
         if GIDSignIn.sharedInstance()?.currentUser != nil {
             
@@ -189,61 +193,48 @@ class ViewController: UIViewController, GIDSignInDelegate {
         
     }
     
-    @IBAction func getAssignments(_ sender: Any) {
-
-        if GIDSignIn.sharedInstance()?.currentUser != nil {
-            
-            myAuth = GIDSignIn.sharedInstance()?.currentUser.authentication.fetcherAuthorizer()
-        } else {
-            myAuth = nil
-        }
-        service.authorizer = myAuth
-        fetchAssignments()
-        
-    }
-    
-    @IBAction func getAssignmentsPerCourse(_ sender: Any) {
+    @IBAction func showInfo(_ sender: Any) {
         assignmentIndex = 0
         textViewTest.text = ""
-        for i in 0...assignmentsPerCourse.count - 1 {
-            textViewTest.text += "\(assignmentsPerCourse[i].first ?? "no name"): \(assignmentsPerCourse[i].arrayWithoutFirstElement())\n\n"
-
+        if assignmentsPerCourse.count != 0 {
+            for i in 0...assignmentsPerCourse.count - 1 {
+                classNameAndAssignments.updateValue(assignmentsPerCourse[i].arrayWithoutFirstElement(), forKey: assignmentsPerCourse[i].first ?? "no name")
+    
+            }
+            for (key, value) in classNameAndAssignments {
+                textViewTest.text += "\(key):\n\(value)\n\n"
+            }
+        } else {
+            textViewTest.text = "Unable to show info"
         }
-        
     }
     
-    private func addToolbar(_ toolbar: UIToolbar, toView view: UIView) {
-        toolbar.frame = CGRect(x: 0,
-                               y: 0,
-                               width: view.frame.size.width,
-                               height: 0)
-        toolbar.sizeToFit() // This sets the standard height for the toolbar.
-
-        // Create a view to contain the toolbar:
-        let toolbarParent = UIView()
-        toolbarParent.frame = CGRect(x: 0,
-                                     y: view.frame.size.height - toolbar.frame.size.height,
-                                     width: toolbar.frame.size.width,
-                                     height: toolbar.frame.size.height)
-
-        // Adjust the position and height of the toolbar's parent view to account for safe area:
-        if #available(iOS 11, *) {
-            toolbarParent.frame.origin.y -= view.safeAreaInsets.bottom
-            toolbarParent.frame.size.height += view.safeAreaInsets.bottom
-        }
-
-        toolbarParent.addSubview(toolbar)
-        view.addSubview(toolbarParent)
-    }
-}
-
-extension Array {
-    func arrayWithoutFirstElement() -> Array {
-        if count != 0 { // Check if Array is empty to prevent crash
-            var newArray = Array(self)
-            newArray.removeFirst()
-            return newArray
-        }
-        return []
-    }
+        
+    
+    
+//    private func addToolbar(_ toolbar: UIToolbar, toView view: UIView) {
+//        toolbar.frame = CGRect(x: 0,
+//                               y: 0,
+//                               width: view.frame.size.width,
+//                               height: 0)
+//        toolbar.sizeToFit() // This sets the standard height for the toolbar.
+//
+//        // Create a view to contain the toolbar:
+//        let toolbarParent = UIView()
+//        toolbarParent.frame = CGRect(x: 0,
+//                                     y: view.frame.size.height - toolbar.frame.size.height,
+//                                     width: toolbar.frame.size.width,
+//                                     height: toolbar.frame.size.height)
+//
+//        // Adjust the position and height of the toolbar's parent view to account for safe area:
+//        if #available(iOS 11, *) {
+//            toolbarParent.frame.origin.y -= view.safeAreaInsets.bottom
+//            toolbarParent.frame.size.height += view.safeAreaInsets.bottom
+//        }
+//
+//
+//        toolbarParent.addSubview(toolbar)
+//        view.addSubview(toolbarParent)
+//    }
+    
 }
